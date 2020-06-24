3460312ae92b8bf92799d3b31b86cb0495a76b9e
Himanshu Singhal <hsinghal@akamai.com>
2020-06-24 09:59:17 +0530

    added some entities

--- src/main/java/com/devtalk/carparking/dao/entity/StateEntity.java
+++ src/main/java/com/devtalk/carparking/dao/entity/StateEntity.java
@@ -1,23 +1,29 @@
 package com.devtalk.carparking.dao.entity;
 
 import com.devtalk.carparking.model.seeddata.State;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.*;
 
 @Entity
 @Data
 @Table(name = "STATES")
+@NoArgsConstructor
+@AllArgsConstructor
 public class StateEntity {
 
     @Id
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "STATE_NAME")
     private String name;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "STATE_NAME", referencedColumnName = "STATE_NAME")
+    private List<CityEntity> cityEntityList;
 
     public static State getStateFromStateEntity(StateEntity stateEntity){
         State state= new State();
